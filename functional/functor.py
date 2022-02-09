c816c052e087ae874bf3e7780040d3ddb2a5b724
MullerWlad <57362446+MullerWlad@users.noreply.github.com>
2022-02-09 16:39:09 +0300

    Update functor.py

--- functional/functor.py
+++ functional/functor.py
@@ -1,9 +1,36 @@
 from functional.composition import o
+
+class Context:
+    def __init__(self, function: object, object: object):
+        self.f = function
+        self.obj = object
+    def __enter__(self):
+        self.message = "Context created for " + str(self.f) + " " + str(self.obj)
+        try:
+            return self.f(self.obj)
+        except:
+            self.message = "Context failed for " + str(self.f) + " " + str(self.obj)
+    def __exit__(self, type, val, trace):
+        print(self.message)
+
 class Functor:
     def __init__(self, functor: object, function: object):
         self.F = functor
         self.f = function
     def __call__(self, object):
-        return self.F(object)
+        with Context(self.F, object) as answer:
+            return answer
     def Fof(self, object):
-        return o([self.F, self.f])(object)
+        with Context(o([self.F, self.f]), object) as answer:
+            return answer
+
+class FQuantity:
+    def __Fq(qua: list, container: object) -> list:
+        match qua:
+            case []:
+                return []
+            case qua:
+                return [container(qua[0])] + FQuantity.__Fq(qua[1:], container)
+    def __init__(self, qua: list, functor: Functor):
+        self.Fa = FQuantity.__Fq(qua, functor)
+        self.Fb = FQuantity.__Fq(qua, functor.Fof)
